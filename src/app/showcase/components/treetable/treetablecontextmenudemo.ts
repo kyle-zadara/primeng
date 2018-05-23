5e79baf559e3e2eb4fc5664329c43a62afc3c277
kyle-zadara <kyle@zadarastorage.com>
2018-05-23 13:39:48 -0700

    switch to npm code

--- src/app/showcase/components/treetable/treetablecontextmenudemo.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    templateUrl: './treetablecontextmenudemo.html'
-})
-export class TreeTableContextMenuDemo {
-
-}
--- src/app/showcase/components/treetable/treetablereorderdemo.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    templateUrl: './treetablereorderdemo.html'
-})
-export class TreeTableReorderDemo {
-
-}
